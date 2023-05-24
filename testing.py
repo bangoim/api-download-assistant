3e2d97aa70f15beae4a00be03e59fc7da94e820e
João Castello Branco <129008919+bangoim@users.noreply.github.com>
2023-05-24 17:41:29 -0300

    Update testing.py
    Added the option to select 2 types of base URLs. Still testing.

--- testing.py
+++ testing.py
@@ -6,24 +6,34 @@ from urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
-base_url = "http://api.esportsearnings.com/v0/LookupHighestEarningPlayers"
-apikey = "7ccb2bf499c272fc4c3995ce7be04e772ab97c5677e184a426e2d94e2400420a"
-directory = "C:\\Users\\joaoc\\OneDrive\\Área de Trabalho\\eSports Project\\"
-
+apikey = "<your_api_key_here>"
+directory = "<your_directory_here>"
 os.makedirs(directory, exist_ok=True)
-
 last_offset_file = os.path.join(directory, "last_offset.txt")
-if os.path.exists(last_offset_file):
-    with open(last_offset_file, "r") as file:
-        offset = int(file.read()) + 100
-else:
-    offset = 0
-
 output_file = os.path.join(directory, base_url.split("/")[-1] + ".csv")
+
 failed_offsets = []
 
+option = int(input("Select an option:\n1. LookupHighestEarningPlayers\n2. LookupPlayerById\n"))
+if option == 1:
+    base_url = "http://api.esportsearnings.com/v0/LookupHighestEarningPlayers"
+    increment = 100
+elif option == 2:
+    base_url = "http://api.esportsearnings.com/v0/LookupPlayerById"
+    increment = 1
+else:
+    print("Invalid option.")
+    exit()
+
+if os.path.exists(last_offset_file):
+    with open(last_offset_file, "r") as file:
+        offset = int(file.read()) + increment
+else:
+    offset = 0 if option == 1 else 1000
+
 while True:
-    url = f"{base_url}?apikey={apikey}&offset={offset}&format=csv"
+    url = f"{base_url}?apikey={apikey}&offset={offset}&format=csv" if option == 1 else f"{base_url}?apikey={apikey}&playerid={offset}&format=csv"
+
     try:
         response = requests.get(url, verify=False)
         if response.status_code != 200 or not response.text.strip():
@@ -31,13 +41,18 @@ while True:
         temp_file = os.path.join(directory, f"temp_{offset}.csv")
         with open(temp_file, "w", encoding='utf-8') as file:
             file.write(response.text)
+
         df = pd.read_csv(temp_file)
+
         if os.path.exists(output_file):
             df.to_csv(output_file, mode='a', header=False, index=False)
         else:
             df.to_csv(output_file, mode='w', index=False)
+
         os.remove(temp_file)
+
         print(f"Downloaded offset: {offset}")
+
         with open(last_offset_file, "w") as file:
             file.write(str(offset))
 
@@ -45,7 +60,7 @@ while True:
         print(f"Failed to download offset: {offset}")
         failed_offsets.append(offset)
 
-    offset += 100
+    offset += increment
     time.sleep(3)
 
 if failed_offsets:
